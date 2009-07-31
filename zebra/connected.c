3241ec23275443eb10791aed6d6cc9b3a9388198
Balaji <balaji@localhost.localdomain>
2009-07-31 22:37:29 +0530

    Fixed Connected routes deletion bug in M-RIB table on async events.

--- zebra/connected.c
+++ zebra/connected.c
@@ -301,6 +301,11 @@ connected_down_ipv4 (struct interface *ifp, struct connected *ifc)
   /* Same logic as for connected_up_ipv4(): push the changes into the head. */
   rib_delete_ipv4 (ZEBRA_ROUTE_CONNECT, 0, &p, NULL, ifp->ifindex, 0, SAFI_UNICAST);
 
+  /*
+   * The multicast connected routes should get deleted on async events.
+   */
+  rib_delete_ipv4 (ZEBRA_ROUTE_CONNECT, 0, &p, NULL, ifp->ifindex, 0, SAFI_MULTICAST);
+
   rib_update ();
 }
 
